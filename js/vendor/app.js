9f224a840ab69ff6839733522163781641ae61d5
Isaac Bueno <isaacmabueno@gmail.com>
2015-10-26 14:15:30 -0500

    added bands in town api

--- js/vendor/app.js
+++ js/vendor/app.js
@@ -2,3 +2,14 @@ $(document).ready(function(){
 $(".main-logo").fadeIn(5000);
 
 });
+$(documet).ready(function(){
+  
+  "name": "The Pact",
+  "image_url": "http://www.bandsintown.com/thepact/photo/medium.jpg",
+  "thumb_url": "http://www.bandsintown.com/thepact/photo/small.jpg",
+  "facebook_tour_dates_url": "http://bnds.in/jrHVeT",
+  "mbid": "cbfb9bcd-c5a0-4d7c-865f-2c641c171e1c",
+  "upcoming_events_count": 7,
+  "tracker_count": 245345
+
+})
